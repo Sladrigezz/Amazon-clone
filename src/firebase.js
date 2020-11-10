252d3ba293cd2c4fedfaab6ed5a8f54dad1d62cc
Sladrigezz <Sladrigezz@github.com>
2020-11-10 20:18:06 +0300

    start

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDsLOv5RY8RcsbwT4Tzfcc7P2nFM9F490k",
+  authDomain: "clone-6cb46.firebaseapp.com",
+  databaseURL: "https://clone-6cb46.firebaseio.com",
+  projectId: "clone-6cb46",
+  storageBucket: "clone-6cb46.appspot.com",
+  messagingSenderId: "534510822846",
+  appId: "1:534510822846:web:49fc7a5004c2aec7a385ef"
+};
